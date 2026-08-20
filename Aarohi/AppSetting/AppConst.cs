5c65d5e68d43f3783c85b0fede87e11dbf953003
abhishekdabhade <abhishek.dabhade02@gmail.com>
2026-08-20 13:51:10 +0530

    Add app configuration constants
    Add a shared AppConst class under Aarohi.AppSetting to centralize the application name and version. This provides a simple configuration source for app metadata used across the project.

---
+++ Aarohi/AppSetting/AppConst.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Aarohi.AppSetting
+{
+    public class AppConst
+    {
+        public static string AppName = "Aarohi_Core";
+        public static string AppVersion = "1.0.0";
+    }
+}
